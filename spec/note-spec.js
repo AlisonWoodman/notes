2751ae59276b9bd82d30a16f58b9a993beb6e331
Alison Woodman <aliwoodman@gmail.com>
2017-08-14 14:26:54 +0100

    Note list model added

--- spec/note-spec.js
+++ spec/note-spec.js
@@ -3,3 +3,11 @@
   note = new Note('Javascript is great');
   assert.isTrue(note.text() === 'Javascript is great', description);
 })();
+
+(function() {
+  var description = "Note list stores array of notes";
+  noteList = new  NoteList();
+  noteList.addNote("Testing is fun")
+  assert.isTrue(noteList.getNotes().length === 1, description);
+  assert.isTrue(noteList.getNotes()[0].text() === "Testing is fun", description);
+})();
---
+++ src/note-list-model.js
@@ -0,0 +1,11 @@
+function NoteList (){
+  this._notes = []
+}
+
+NoteList.prototype.addNote = function(text) {
+  this._notes.push(new Note(text));
+};
+
+NoteList.prototype.getNotes = function(){
+  return this._notes
+};
