e59711fd05846036566338aa0f08cc6538ea447c
Ryan Davidson <daviryan@aol.com>
2017-08-15 15:57:27 +0100

    Test instanceof Note Controller

--- spec/note-controller-spec.js
+++ spec/note-controller-spec.js
@@ -1,4 +1,11 @@
 (function(){
+  var description = "Instantiate a new note controller";
+  var noteController = createNoteController();
+  assert.isTrue((noteController instanceof NoteController), description);
+})();
+
+
+(function(){
   var description = "Note controller #displayNoteList sets innerHTML to correct HTML";
   var NoteController = createNoteController();
   NoteController.displayNoteList();
