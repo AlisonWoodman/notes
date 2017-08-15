652ac9bf65b2ad393f434989819f35581eead000
Ryan Davidson <daviryan@aol.com>
2017-08-15 13:01:34 +0100

    Developing add html list to page

--- controllers/note-controller.js
+++ controllers/note-controller.js
@@ -1,1 +1,16 @@
-document.getElementById("app").innerHTML = "Howdy";
+//document.getElementById("app").innerHTML = "Howdy";
+
+(function(exports){
+  function NoteController(noteListModel) {
+    this.list = new noteListModel();
+    this.list.addNote("Favourite drink: seltzer");
+    this.noteListView = new NoteListView(this.list);
+  }
+  NoteController.prototype.displayNoteList = function () {
+    document.getElementById("app").innerHTML = this.noteListView.display();
+  };
+  exports.NoteController = NoteController;
+})(this);
+
+console.log(new NoteController(NoteList));
+document.getElementById("app").innerHTML = new NoteController(NoteList).displayNoteList();
